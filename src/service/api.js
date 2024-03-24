47021d517c1612942140c939777f80770b7cae95
Muhammad Bilal <bilal.soft000@gmail.com>
2024-03-24 07:33:15 +0500

    keys in env variables

--- src/service/api.js
+++ src/service/api.js
@@ -1,5 +1,7 @@
 import { getDateWithoutDashes } from "../utils";
-
+const NYT_API_KEY = import.meta.env.VITE_NYT_API_KEY
+const GUADIAN_API_KEY = import.meta.env.VITE_GUADIAN_API_KEY
+const NEWS_API_KEY = import.meta.env.VITE_NEWS_API_KEY
 
 const api = {
   getNewYourTimesArticles: async ({ query = "", startDate = "", endDate = "" } = {}) => {
@@ -7,7 +9,7 @@ const api = {
       const beginDate = getDateWithoutDashes(startDate);
       const toDate = getDateWithoutDashes(endDate);
       var url = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${query}${startDate ? `&begin_date=${beginDate}` : ""}${endDate ? `&end_date=${toDate}` : ""
-        }&api-key=czEGGWR42TG7mCBbTscLGBgAO3TKeKCJ`;
+        }&api-key=${NYT_API_KEY}`;
       const res = await fetch(url);
       const data = await res.json();
       return data
@@ -18,7 +20,7 @@ const api = {
   getGuadianArticles: async ({ query = "", startDate = "", endDate = "" } = {}) => {
     try {
       var url = `https://content.guardianapis.com/search?q=${query}${startDate ? `&from-date=${startDate}` : ""}${endDate ? `&to-date=${endDate}` : ""
-        }&show-tags=contributor&api-key=18e0677c-4348-4c45-a357-a29a2eb04911`;
+        }&show-tags=contributor&api-key=${GUADIAN_API_KEY}`;
       const res = await fetch(url);
       const data = await res.json();
       return data
@@ -34,7 +36,7 @@ const api = {
         `${startDate ? `from=${startDate}&` : ""}` +
         `${endDate ? `to=${endDate}&` : ""}` +
         "sortBy=popularity&" +
-        "apiKey=4879cb53518145099b220a86f4aa3f20";
+        `apiKey=${NEWS_API_KEY}`;
       const res = await fetch(url);
       const data = await res.json();
       return data
